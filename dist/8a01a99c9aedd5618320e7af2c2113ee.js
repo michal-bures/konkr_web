6628369192aa0bda33da94f72da9510eb03165c0
Michal Bureš <mbures01@gmail.com>
2017-01-22 21:25:52 +0100

    game update

---
+++ dist/8a01a99c9aedd5618320e7af2c2113ee.js
@@ -0,0 +1,85 @@
+{
+"frames": {
+	"btn-fullscreen": {
+		"frame": {"x":34, "y":190, "w":33, "h":33},
+		"spriteSourceSize": {"x":0,"y":0,"w":33,"h":33},
+		"sourceSize": {"w":33,"h":33}
+	},
+	"btn-restart": {
+		"frame": {"x":68, "y":175, "w":33, "h":33},
+		"spriteSourceSize": {"x":0,"y":0,"w":33,"h":33},
+		"sourceSize": {"w":33,"h":33}
+	},
+	"btn-sound-off": {
+		"frame": {"x":0, "y":190, "w":33, "h":33},
+		"spriteSourceSize": {"x":0,"y":0,"w":33,"h":33},
+		"sourceSize": {"w":33,"h":33}
+	},
+	"btn-sound-on": {
+		"frame": {"x":37, "y":141, "w":33, "h":33},
+		"spriteSourceSize": {"x":0,"y":0,"w":33,"h":33},
+		"sourceSize": {"w":33,"h":33}
+	},
+	"coin": {
+		"frame": {"x":68, "y":81, "w":11, "h":15},
+		"spriteSourceSize": {"x":0,"y":0,"w":11,"h":15},
+		"sourceSize": {"w":11,"h":15}
+	},
+	"flag-green": {
+		"frame": {"x":0, "y":141, "w":36, "h":48},
+		"spriteSourceSize": {"x":0,"y":0,"w":36,"h":48},
+		"sourceSize": {"w":36,"h":48}
+	},
+	"goldCoins": {
+		"frame": {"x":0, "y":224, "w":24, "h":24},
+		"spriteSourceSize": {"x":0,"y":0,"w":24,"h":24},
+		"sourceSize": {"w":24,"h":24}
+	},
+	"might": {
+		"frame": {"x":66, "y":224, "w":11, "h":15},
+		"spriteSourceSize": {"x":0,"y":0,"w":11,"h":15},
+		"sourceSize": {"w":11,"h":15}
+	},
+	"nextTurnButton": {
+		"frame": {"x":0, "y":81, "w":67, "h":29},
+		"spriteSourceSize": {"x":0,"y":0,"w":67,"h":29},
+		"sourceSize": {"w":67,"h":29}
+	},
+	"paneBackground": {
+		"frame": {"x":25, "y":241, "w":5, "h":5},
+		"spriteSourceSize": {"x":0,"y":0,"w":5,"h":5},
+		"sourceSize": {"w":5,"h":5}
+	},
+	"popoverPointer": {
+		"frame": {"x":25, "y":224, "w":16, "h":16},
+		"spriteSourceSize": {"x":0,"y":0,"w":16,"h":16},
+		"sourceSize": {"w":16,"h":16}
+	},
+	"regionPanel": {
+		"frame": {"x":0, "y":0, "w":248, "h":80},
+		"spriteSourceSize": {"x":0,"y":0,"w":248,"h":80},
+		"sourceSize": {"w":248,"h":80}
+	},
+	"shield-blue": {
+		"frame": {"x":54, "y":224, "w":11, "h":15},
+		"spriteSourceSize": {"x":0,"y":0,"w":11,"h":15},
+		"sourceSize": {"w":11,"h":15}
+	},
+	"shield-red": {
+		"frame": {"x":42, "y":224, "w":11, "h":15},
+		"spriteSourceSize": {"x":0,"y":0,"w":11,"h":15},
+		"sourceSize": {"w":11,"h":15}
+	},
+	"undoButton": {
+		"frame": {"x":0, "y":111, "w":67, "h":29},
+		"spriteSourceSize": {"x":0,"y":0,"w":67,"h":29},
+		"sourceSize": {"w":67,"h":29}
+	}
+
+},
+"meta": {
+	"image": "../../sprites.png",
+	"size": {"w": 249, "h": 249},
+	"scale": "1"
+}
+}
