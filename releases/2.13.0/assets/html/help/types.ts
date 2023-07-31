189110994182faf1cc914a44c7b99a9941a15aad
michal-bures <embaircz@gmail.com>
2023-07-31 20:59:17 +0200

    purge old versions

--- releases/2.13.0/assets/html/help/types.ts
+++
@@ -1,8 +0,0 @@
-export type HelpPageKey =
-    | 'tutorial-goal'
-    | 'tutorial-economy'
-    | 'tutorial-conquest'
-    | 'tutorial-politics'
-    | 'tutorial-bandits'
-    | 'tutorial-strategy-tips'
-    | 'tutorial-advanced-controls'
